c0af86b5556da414db1bb561ba384fe4abbb867d
sergiodm92 <crgiodm@gmail.com>
2023-05-04 16:10:48 -0300

    build

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import axios from 'axios';
-import store from './redux/store'
+import store from './redux/store.js'
 import App from './App';
 
 
