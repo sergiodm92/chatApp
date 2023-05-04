07b169678fee609176fa92da24b167578744c403
sergiodm92 <crgiodm@gmail.com>
2023-05-04 17:37:19 -0300

    store

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import axios from 'axios';
 import App from './App';
-import store from './redux/store'
+import store from './redux/store';
+
 
 axios.defaults.baseURL = 'http://localhost:3001'
 
