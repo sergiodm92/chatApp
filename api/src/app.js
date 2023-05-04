ad93f20d28ddb3b795bd16ffd4ee11a81850e1cb
sergiodm92 <crgiodm@gmail.com>
2023-05-04 20:53:19 -0300

    asd

--- api/src/app.js
+++ api/src/app.js
@@ -7,24 +7,6 @@ require('./db.js');
 
 const app = express();
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-
-io.on('connection', (socket) => {
-  console.log('Nuevo cliente conectado');
-
-  // maneja el evento "mensaje-nuevo" enviado por el cliente
-  socket.on('mensaje-nuevo', (data) => {
-    console.log(data.mensaje);
-    // envía el mensaje a todos los clientes conectados
-    io.emit('mensaje-recibido', data);
-
-    // envía un mensaje específico al cliente que envió el mensaje original
-    socket.emit('reload', 'reload');
-  });
-});
-
-
 server.name = 'API';
 
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
