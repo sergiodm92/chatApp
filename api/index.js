220d2cc42d46cf542684b3eba3fdf6ebb22bf84d
sergiodm92 <crgiodm@gmail.com>
2023-05-05 17:54:38 -0300

    asd

--- api/index.js
+++ api/index.js
@@ -1,13 +1,25 @@
-const server = require('./src/app.js');
+const app = require('./src/app.js');
 const { conn} = require('./src/db.js');
-
+const { Server } = require('socket.io');
 const PORT = process.env.PORT || 3001; 
+const  http  = require('http');
+
+
+const server = http.createServer(app)
+const io = new Server(server,{
+  cors:{
+    origin: 'http://localhost:3000/',
+  }
+})
+
+io.on('connection',()=>{
+  console.log('a user connected')
+})
 
 
 // Syncing all the models at once.
 conn.sync().then(() => {
-  server.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
- 
 });
